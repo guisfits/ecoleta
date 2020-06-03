d61fcef1f3e58ab08c383d408b6c68ff31abf07d
Guilherme Camargo <guisfits@hotmail.com>
2020-06-02 22:36:08 -0300

    knexfile and connection

---
+++ server/knexfile.ts
@@ -0,0 +1,17 @@
+import path from "path";
+
+module.exports = {
+    client: "pg",
+    connection: {
+        host :      'localhost',
+        database:   'ecoleta-db',
+        user:       'server',
+        password:   'rocketseat'
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds')
+    }
+};
