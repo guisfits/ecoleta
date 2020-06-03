3d2a699b4988a0df6a62d29dd9383115a86432f0
Guilherme Camargo <guisfits@hotmail.com>
2020-06-03 01:13:57 -0300

    insert point

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -15,4 +15,48 @@ routes.get('/items', async (request, response) => {
     return response.json(serializedItems);
 });
 
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    try {
+        const trx = await knex.transaction();
+
+        const insertedIds = await trx('points')
+            .insert({
+                image: 'image-fake',
+                name,
+                email,
+                whatsapp,
+                latitude,
+                longitude,
+                city,
+                uf
+            })
+            .returning("id")
+            .into("points");
+
+        const point_id = insertedIds[0];
+
+        const pointItems = items.map((item_id: number) => {
+            return { item_id, point_id };
+        });
+
+        await trx('points-items').insert(pointItems);
+
+        return response.status(201).json({ success: true });
+    } 
+    catch (err) {
+        return response.status(500).json({ success: false, error: err });
+    }
+})
+
 export default routes;
