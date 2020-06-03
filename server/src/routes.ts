f0931f17d12f104e430c3d9315932436e16be9c3
Guilherme Camargo <guisfits@hotmail.com>
2020-06-03 01:43:15 -0300

    rename index point-controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,7 +6,7 @@ const routes = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get('/items', itemsController.Get);
+routes.get('/items', itemsController.index);
 routes.post('/points', pointsController.create);
 
 export default routes;
