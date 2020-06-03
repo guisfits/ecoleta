d7ee68cbd6fadf051732bf15b8ee88a480356e23
Guilherme Camargo <guisfits@hotmail.com>
2020-06-02 23:07:09 -0300

    getting image from server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+
+app.use('/public', express.static(path.resolve(__dirname, '..', 'public', 'images')));
+
 app.listen(3333)
