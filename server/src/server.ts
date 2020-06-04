028a5bba04d5fe5fc27b0a4d442e413cc2995667
Guilherme Camargo <guisfits@hotmail.com>
2020-06-03 23:55:08 -0300

    images path

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import routes from "./routes";
 import path from "path";
-import cors from "cors";
+// import cors from "cors";
 
 const app = express();
 
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use('/public', express.static(path.resolve(__dirname, '..', 'public', 'images')));
+app.use('/', express.static(path.resolve(__dirname, '..', 'public', 'images')));
 
-app.listen(3333)
+app.listen(3333);
