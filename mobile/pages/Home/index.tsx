8fc338689b47c0022faa8380c2f05d6c0d21a30a
Guilherme Camargo <guisfits@hotmail.com>
2020-06-05 21:02:00 -0300

    home page

--- mobile/pages/Home/index.tsx
+++ mobile/pages/Home/index.tsx
@@ -1,7 +1,40 @@
+import React from 'react'
+import { Feather as Icon } from '@expo/vector-icons'
+import { Image, ImageBackground, View, Text, StyleSheet } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler'
+
+const Home = () => {
+  return (
+    <ImageBackground
+      source={require("../../assets/home-background.png")}
+      style={styles.container}
+      imageStyle={{ width: 274, height: 368 }} >
+      <View style={styles.main} >
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.title}>Seu market place de coleta de resíduous</Text>
+        <Text style={styles.description}>Ajudamos pessoas a enontrarem pontos de coletas de forma eficiente.</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => { }}>
+          <View style={styles.buttonIcon}>
+            <Icon name="arrow-right" color="#FFF" size={24}></Icon>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
+    backgroundColor: "#f0f0f5"
   },
 
   main: {
@@ -66,3 +99,5 @@ const styles = StyleSheet.create({
     fontSize: 16,
   }
 });
+
+export default Home;
